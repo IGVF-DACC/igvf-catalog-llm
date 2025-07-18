bb59bdc9c7860133267f25bb185f085a1fad7902
Otto Jolanki <ojolanki@stanford.edu>
2025-07-18 15:12:13 -0700

    try using catalog.igvf.org domain

--- cdk/infrastructure/constructs/existing/catalog_llm_dev.py
+++ cdk/infrastructure/constructs/existing/catalog_llm_dev.py
@@ -3,7 +3,7 @@ from aws_cdk.aws_ec2 import Vpc
 
 from shared_infrastructure.igvf_dev.connection import CodeStarConnection
 from shared_infrastructure.igvf_dev.environment import US_WEST_2 as US_WEST_2
-from shared_infrastructure.igvf_dev.domain import DemoDomain
+#from shared_infrastructure.igvf_dev.domain import DemoDomain
 from shared_infrastructure.igvf_dev.secret import DockerHubCredentials
 from shared_infrastructure.igvf_dev.notification import Notification
 from shared_infrastructure.igvf_dev.bus import Bus
@@ -11,6 +11,30 @@ from shared_infrastructure.igvf_dev.bus import Bus
 
 
 from typing import Any
+
+from constructs import Construct
+
+from aws_cdk.aws_certificatemanager import Certificate
+from aws_cdk.aws_route53 import HostedZone
+
+from typing import Any
+
+
+class DemoDomain(Construct):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs: Any) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        self.name = 'catalog.igvf.org'
+        self.certificate = Certificate.from_certificate_arn(
+            self,
+            'DomainCertificate',
+            'arn:aws:acm:us-west-2:109189702753:certificate/84e0ade9-fb95-49e9-bf38-fb663cc38d55'
+        )
+        self.zone = HostedZone.from_lookup(
+            self,
+            'DomainZone',
+            domain_name=self.name,
+        )
 
 
 class Resources(Construct):
